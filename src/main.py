0ee3d7a3a4d6cae6bea4cebbf69b3ca80c4681a5
chanhui <kinm1596@daum.net>
2022-10-30 08:33:25 +0000

    10.30 now vita can speak korean

--- src/main.py
+++ src/main.py
@@ -2,12 +2,12 @@ import time
 import threading
 from pytz import timezone
 from datetime import date, datetime
-from tts import saying
+from ttsKorean import saying
 from speechToText import speechToText
 
 alarmList = [
-		{'type': 'C', 'date': datetime(2022,10,27,21,49)},
-		{'type': 'B2', 'date': datetime(2022, 10,27,21,40)},
+		{'type': 'C', 'date': datetime(2022,10,30,17,32)},
+		{'type': 'B2', 'date': datetime(2022,10,30,16,37)},
 		{'type': 'B1', 'date': datetime(2022, 10, 27, 21, 37)}
 	]
 
@@ -16,16 +16,16 @@ def conversation():
 	print('last 40 sec...')
 	for i in alarmList:
 		if now.year == i['date'].year and now.month == i['date'].month and now.day == i['date'].day and  now.hour == i['date'].hour and now.minute == i['date'].minute:
-			saying('alarm')
+			saying('비타민 드셨나요?')
 			client_answer = speechToText()
 			if client_answer == 'yes':
-				saying('good')
+				saying('잘하셨어요')
 			elif client_answer == 'no':
-				saying('not good')
+				saying('비타민을 복약해야합니다.')
 			elif client_answer == 'hello':
-				saying('hello')
+				saying('안녕하세요 비타입니다.')
 			elif client_answer == 'bye':
-				saying('ok bye')
+				saying('종료합니다.')
 				exit()
 
 def alarm():
